c66f4354d9f4ea15fa53ceea8719e591cf53c974
mattcmerritt <mattcmerritt@gmail.com>
2021-12-04 16:58:31 -0500

    added a laser to show where the player is aiming

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    private List<Enemy> Enemies;
+    public List<Vector3> StartPositions;
+
+    private void Awake()
+    {
+        
+    }
+}
