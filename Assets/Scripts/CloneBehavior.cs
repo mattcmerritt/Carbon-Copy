90f509b574336841fd9f310b90b2a6258ec711e0
mattcmerritt <mattcmerritt@gmail.com>
2021-12-06 04:59:09 -0500

    weapon collection fixes

--- Assets/Scripts/CloneBehavior.cs
+++ Assets/Scripts/CloneBehavior.cs
@@ -10,6 +10,8 @@ public class CloneBehavior : MonoBehaviour
 
     public int CloneIndex;
 
+    private float WeaponSwapCooldown;
+
     private void Awake()
     {
         MaxHealth = Health;
@@ -27,6 +29,7 @@ public class CloneBehavior : MonoBehaviour
                 }
             }    
         }
+        WeaponSwapCooldown += Time.deltaTime;
     }
 
    public void Collect(GameObject collectible)
@@ -44,6 +47,9 @@ public class CloneBehavior : MonoBehaviour
             weapon.SetClone(gameObject);
             CurrentWeapon = weapon;
         }
+
+        // reset weapon cooldown
+        WeaponSwapCooldown = 0f;
    }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -104,5 +110,15 @@ public class CloneBehavior : MonoBehaviour
     public float GetHealthPercent()
     {
         return Health / MaxHealth;
+    }
+
+    public bool HasWeapon()
+    {
+        return CurrentWeapon != null;
+    }
+
+    public bool CanGrabWeapon()
+    {
+        return WeaponSwapCooldown > 0.5f;
     }
 }
--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            transform.SetParent(collision.transform);
-            Collider2D collider = GetComponent<Collider2D>();
-            collider.enabled = false;
-            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
-            renderer.enabled = false;
-            this.enabled = false;
+            if (!collision.GetComponent<CloneBehavior>().HasWeapon())
+            {
+                transform.SetParent(collision.transform);
+                Collider2D collider = GetComponent<Collider2D>();
+                collider.enabled = false;
+                SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+                renderer.enabled = false;
+                this.enabled = false;
 
-            CloneBehavior clone = collision.GetComponent<CloneBehavior>();
-            clone.Collect(gameObject);
+                CloneBehavior clone = collision.GetComponent<CloneBehavior>();
+                clone.Collect(gameObject);
+            }
+            else if (Input.GetKey(KeyCode.E) && collision.GetComponent<CloneBehavior>().CanGrabWeapon())
+            {
+                transform.SetParent(collision.transform);
+                Collider2D collider = GetComponent<Collider2D>();
+                collider.enabled = false;
+                SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+                renderer.enabled = false;
+                this.enabled = false;
+
+                CloneBehavior clone = collision.GetComponent<CloneBehavior>();
+                clone.Collect(gameObject);
+            }
         }
     }
 }
